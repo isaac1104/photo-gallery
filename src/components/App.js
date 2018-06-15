3684b72c5aa093e2f902fef2ee4303be5f4fb98e
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-15 09:26:10 -0700

    App component changed to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,16 @@ import Photos from './Photos';
 import logo from '../logo.svg';
 import '.././styles/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Photos />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <Photos />
+    </div>
+  );
 }
 
 export default App;
